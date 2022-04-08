706f4fdcb09b01f6d81c146cda82b63525400209
Nemes1sX <kazstonk@gmail.com>
2022-04-08 23:01:45 +0300

    Add project files.

---
+++ Temperature/Controllers/TemperatureController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Temperature.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TemperatureController : ControllerBase
+    {
+        [HttpGet]
+        public async Task KelvinFarenheit(int tempc)
+        {
+
+        }
+    }
+}
