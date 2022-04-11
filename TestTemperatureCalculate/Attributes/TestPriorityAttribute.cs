2e535d307f112d9e1b07732265472bdec6b8f3d9
Nemes1sX <kazstonk@gmail.com>
2022-04-11 23:05:27 +0300

    Add testing order

---
+++ TestTemperatureCalculate/Attributes/TestPriorityAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestTemperatureCalculate.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class TestPriorityAttribute : Attribute
+    {
+        public int Priority { get; private set; }
+
+        public TestPriorityAttribute(int priority) => Priority = priority;
+    }
+}
